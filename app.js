3026551a137e68f89c8c4d84ce45ddaf48f19ed7
Ansh Singh <87262109+anshs02@users.noreply.github.com>
2022-08-14 23:54:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/* Name: Ansh Singh Sachdev
+Student Number: 156187213
+Email: asachdev2@myseneca.ca
+Section: ZBB */
+
+
 function ShowHideDiv(obj) {
     var hiring = document.getElementById("hiring");
     var rateTxt = document.getElementById("rateTxt");
